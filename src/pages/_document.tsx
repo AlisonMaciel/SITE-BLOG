0aac6534e438c3eed555accba33c216b2931f575
Alison <alisonbetini27@gmail.com>
2025-10-22 11:46:38 -0300

    adding next favicon to pages and app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="icon" href="/assets/Next.svg" />
+      </Head>
       <body className="antialiased min-h-full bg-[#101216]">
         <Main />
         <NextScript />
